b8f76b7ae1dc5ae4cd599cf1517017946172df7d
ALEXIS RAMON <ALEXIS RAMON@LAPTOP-BP1RR3P1>
2024-04-26 21:39:23 -0600

    Agregar archivos de proyecto.

---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+
+    }
+}
